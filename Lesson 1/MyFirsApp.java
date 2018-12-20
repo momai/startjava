a570ec0e0aaa53261384def853b4b2115b220177
momai <mamai2010@gmail.com>
2018-12-20 15:07:18 +0300

    первые уроки

---
+++ Lesson 1/MyFirsApp.java
@@ -0,0 +1,5 @@
+public class MyFirsApp {
+	public static void main (String [] args){
+		System.out.println("hello, world, yeah bitch");
+	}
+}
---
+++ Lesson 1/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+	public static void main (String [] args){
+	byte core = 4;
+	double coreSpeed = 3.8;
+	byte ram = 8;
+	short storage = 1000;
+	
+	System.out.println("PC info: ");
+    System.out.println("_________");
+	System.out.println("ядер: " + core);
+	System.out.println("„астота: " + coreSpeed);
+	System.out.println("ќ«”: " + ram);
+	System.out.println("HDD: " + storage);
+	}
+}
