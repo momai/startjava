4cbc2ae05b060c7ef762e39bb941c99b1717dde6
momai <mamai2010@gmail.com>
2018-12-20 15:36:22 +0300

    ~ДЗ-1 6 часть урока  add boolean

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,15 +1,23 @@
-﻿public class Variable {
+public class Variable {
 	public static void main (String [] args){
 	byte core = 4;
 	double coreSpeed = 3.8;
 	byte ram = 8;
 	short storage = 1000;
-	
+	boolean bits = false;
+
+
 	System.out.println("PC info: ");
     System.out.println("_________");
 	System.out.println("Ядер: " + core);
-	System.out.println("Частота: " + coreSpeed);
-	System.out.println("ОЗУ: " + ram);
-	System.out.println("HDD: " + storage);
+	System.out.println("Частота: " + coreSpeed + "GHz");
+	System.out.println("ОЗУ: " + ram + "Gb");
+	System.out.println("HDD: " + storage + "Gb");
+	if(bits == true) {
+		System.out.println("system x32");	
+			}
+	else if(bits == false) {
+		System.out.println("system x64");
+			}
 	}
-} 
+}
