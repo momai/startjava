2fc3bd4a60410095ad71390aee45d64fc53cfe97
momai <mamai2010@gmail.com>
2018-12-24 16:12:35 +0300

    fix

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -2,30 +2,28 @@ public class Cycle {
 	public static void main(String[] args) {
 		
 		// вывести от 0 до 20
-			for(int w = 0; w <= 20; w++) {
-				System.out.println(w);
+			for(int i = 0; i <= 20; i++) {
+				System.out.println(i);
 			}
 			System.out.println("__________________");
 
 		// числа от 6 до -6 (шаг итерации 2)
-			int h = 6;
-			while (h >= -6) {
-				System.out.println(h);
-				h-=2;
+			int k = 6;
+			while (k >= -6) {
+				System.out.println(k);
+				k-=2;
 			}
 			System.out.println("__________________");
 
 		// сумма всех нечетных чисел от 10 до 20
-			int i = 10;
+			int j = 10;
 			int sum = 0;
-			do {
-				if(i % 2 == 0) {
-				}
-				else{
-					sum = sum + i;			
-				}			
-				i++;
-			} while (i <= 20);
+			do {			
+				if(j % 2 != 0) {
+					sum = sum + j;
+				}					
+				j++;
+			} while (j <= 20);
 			System.out.println(sum);
 			System.out.println("__________________");
 
