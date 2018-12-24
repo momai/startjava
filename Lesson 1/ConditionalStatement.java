f64cb6e2db1b8fe3bd453cd8e137f82622e05866
momai <mamai2010@gmail.com>
2018-12-24 10:22:30 +0300

    bug 31 line

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,6 +1,40 @@
 public class ConditionalStatement {
-	public static void main (String [] args)
+	public static void main(String[] args) {
+		
+		short year = 2018;
+		short birthday = 1990;
+		char sex = 'M';
+		double height = 1.75;
+		char name = 'А';
+		
+		//почему нельзя использовать short?
+		int age = year - birthday;
+		if(age > 20) {
+			System.out.println("20 лет");
+		}
+
+		if(sex == 'М') {
+			System.out.println("пол: мужской");
+		}
+		if(sex == 'Ж') {
+			System.out.println("пол: женский");
+		}
+
+		if(height < 1.80) {
+			System.out.println("Коротышка");
+		} else {
+			System.out.println("Длинноног");
+		}
+
+		if(name == 'М') {
+			System.out.println("Морти?");
+			else if (name == 'И') {
+				System.out.println("Илон?");
+			} else {
+				System.out.println("Ты не тот кто нужен");
+			}
+		}
 
 
-
-} 
+	}
+}
