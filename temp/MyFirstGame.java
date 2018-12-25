fe31608f67c2ce3dfa2b19cc8ceffc6c2a308781
momai <mamai2010@gmail.com>
2018-12-25 23:39:29 +0300

    add

--- temp/MyFirstGame.java
+++ temp/MyFirstGame.java
@@ -1,12 +1,18 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
-		byte x = 0;
-		byte y = 100;
+		// java
+		byte x = 40;
+		// you
+		byte y = 10;
 
-		byte z = 4;
-
-		if ()
-
-
+		if (x < y) {
+			int diff = y - x;
+			System.out.println("Введенное вами число больше того, что загадал компьютер на " + diff);
+		} else if(x > y) {
+			int diff = x - y;
+			System.out.println("Введенное вами число меньше того, что загадал компьютер на " + diff);
+		}  else if(x == y) {
+			System.out.println("Вы угадали!");
+		}
 	}
 }
