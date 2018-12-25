029a1f9a46d0086a222f2d15fd069d00830cf6d9
momai <mamai2010@gmail.com>
2018-12-25 21:55:55 +0300

    add

--- temp/Calculator.java
+++ temp/Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
 	public static void main(String[] args) {
-		int x = 2;
-		int y = 4;
+		int x = 83;
+		int y = 7;
 		int s = '^';
 
 		int result;
@@ -23,32 +23,23 @@ public class Calculator {
 			result = x / y;
 			System.out.println(result);
 		}
-		// рассчет процента от числа, x число, y процент
+		// рассчет процента от числа, 'x' число, 'y' процент
 		else if (s == '%'){ 
 			result = x * y / 100;
 			System.out.println(result);
 		}
 
-		else if (s == '^'){ 
-			int i = y;
-			int	sum = 0;
-			while (i >= y){
-				sum = x * x;
-				i++;
-				System.out.println(sum);
-			}
-
-
-			// for (int i = 1; i <= y; i++){
-			// 	sum = x * x;
-			// 	System.out.println(sum);
-			// }
-
-
-
-			System.out.println("______");
+		else if (s == '^'){
+			int j = 1;
+			long sum = x;
+			do {			
+				sum = sum * x;
+				j++;
+			} 
+		 	while (j <= y);
+		 	sum = sum / x;
 			System.out.println(sum);
-			}
 		}
-
 	}
+
+}
---
+++ temp/MyFirstGame.java
@@ -0,0 +1,12 @@
+public class MyFirstGame {
+	public static void main(String[] args) {
+		byte x = 0;
+		byte y = 100;
+
+		byte z = 4;
+
+		if ()
+
+
+	}
+}
