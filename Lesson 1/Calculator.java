b618ad31ab47b1166ddee14ad7d44ac06c8bf76d
momai <mamai2010@gmail.com>
2018-12-25 23:08:00 +0300

    fix

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,44 +1,27 @@
 public class Calculator {
 	public static void main(String[] args) {
-		int x = 83;
-		int y = 7;
-		int s = '^';
+		int x = 2;
+		int y = 4;
+		char s = '^';
 
-		int result;
-
-
-		if (s == '+'){
-			result = x + y;
+		if (s == '+') {
+			System.out.println(x + y);
+		} else if(s == '-') {
+			System.out.println(x - y);
+		} else if (s == '*') {
+			System.out.println(x * y);
+		} else if (s == '/') {
+			System.out.println(x / y);
+		} else if (s == '%') { 
+			System.out.println(x%y);
+		} else if (s == '^') {
+			int i = 1;
+			long result = 1;
+			do {
+				result = result * x;
+				i++;
+			} while (i <= y);
 			System.out.println(result);
-		}
-		else if(s == '-'){
-			result = x - y;
-			System.out.println(result);
-		}
-		else if (s == '*'){
-			result = x * y;
-			System.out.println(result);
-		}
-		else if (s == '/'){
-			result = x / y;
-			System.out.println(result);
-		}
-		// расчет процента от числа, 'x' число, 'y' процент
-		else if (s == '%'){ 
-			result = x * y / 100;
-			System.out.println(result);
-		}
-
-		else if (s == '^'){
-			int j = 1;
-			long sum = x;
-			do {			
-				sum = sum * x;
-				j++;
-			} 
-		 	while (j <= y);
-		 	sum = sum / x;
-			System.out.println(sum);
 		}
 	}
 
