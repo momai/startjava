1389b4476634daf8e32e5a7a00458ed9e087c169
momai <mamai2010@gmail.com>
2018-12-25 21:33:53 +0300

    add

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,46 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int x = 83;
+		int y = 7;
+		int s = '^';
+
+		int result;
+
+
+		if (s == '+'){
+			result = x + y;
+			System.out.println(result);
+		}
+		else if(s == '-'){
+			result = x - y;
+			System.out.println(result);
+		}
+		else if (s == '*'){
+			result = x * y;
+			System.out.println(result);
+		}
+		else if (s == '/'){
+			result = x / y;
+			System.out.println(result);
+		}
+		// рассчет процента от числа, 'x' число, 'y' процент
+		else if (s == '%'){ 
+			result = x * y / 100;
+			System.out.println(result);
+		}
+
+		else if (s == '^'){
+			int j = 1;
+			long sum = x;
+			do {			
+				sum = sum * x;
+				j++;
+				// System.out.println(sum);
+			} 
+		 	while (j <= y);
+		 	sum = sum / x;
+			System.out.println(sum);
+		}
+	}
+
+}
