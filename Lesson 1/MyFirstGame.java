158b69faaa531f435f959804f5e713fc46dced1c
momai <mamai2010@gmail.com>
2019-02-01 10:49:46 +0300

    add fix

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -2,31 +2,16 @@ public class MyFirstGame {
     public static void main(String[] args) {
         byte pcNumber = 49;
         byte yourNumber = 48;
-            while (pcNumber != yourNumber) {
-                if (yourNumber < pcNumber) {
-                    System.out.println("Введённое вами число меньше того, что загадал компьютер");
-                    pcNumber--;
-                }
-                else if (yourNumber > pcNumber) {
-                    System.out.println("Введённое вами число БОЛЬШЕ того, что загадал компьютер");
-                    pcNumber++;
-                }
+        while (pcNumber != yourNumber) {
+            if (yourNumber < pcNumber) {
+                System.out.println("Введённое вами число меньше того, что загадал компьютер");
+                pcNumber--;
             }
-                System.out.println("Вы угадали! " + pcNumber);
-            
-            // int i = pcNumber;
-            // while (i != yourNumber) {
-            //     if (yourNumber < i) {
-            //         int diff = i - yourNumber;
-            //         System.out.println("Введённое вами число меньше того, что загадал компьютер на " + diff);
-            //         i--;
-            //     }
-            //     else if (yourNumber > i) {
-            //         int diff = yourNumber - i;
-            //         System.out.println("Введённое вами число БОЛЬШЕ того, что загадал компьютер на " + diff);
-            //         i++;
-            //     }
-            // }
-            //     System.out.println("Вы угадали! " + pcNumber);
+            else if (yourNumber > pcNumber) {
+                System.out.println("Введённое вами число БОЛЬШЕ того, что загадал компьютер");
+                pcNumber++;
+            }
+        }
+        System.out.println("Вы угадали! " + pcNumber);
     }
 }
